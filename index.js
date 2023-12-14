c23c725923b4b1e3e2a3f44cebf1760b671a05d4
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-12-14 23:12:53 +0100

    comander/foo invokeAction create

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+//npm run start:dev
 const { listContacts, getContactById, removeContact, addContact,getContacts } = require('./db/contacts');
 
-//npm run start:dev
+const { Command } = require("commander");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
+function invokeAction({ action, id, name, email, phone }) {
+    const contacts = getContacts();
+
+  switch (action) {
+    case "list":
+        listContacts(contacts);
+      break;
+
+    case "get":
+        getContactById(contacts, id);
+      break;
+
+    case "add":
+        addContact(contacts, name, email, phone);
+      break;
+
+    case "remove":
+        removeContact(contacts, id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
+
